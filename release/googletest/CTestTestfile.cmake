8afff7eb044a1863250e54ac8f04a3a6250d6388
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-05-19 18:18:18 +0200

    added googletest

---
+++ release/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mac/Desktop/projectsInC++/zadanie2SIK/googletest
+# Build directory: /Users/mac/Desktop/projectsInC++/zadanie2SIK/release/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ release/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mac/Desktop/projectsInC++/zadanie2SIK/googletest/googletest
+# Build directory: /Users/mac/Desktop/projectsInC++/zadanie2SIK/release/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
