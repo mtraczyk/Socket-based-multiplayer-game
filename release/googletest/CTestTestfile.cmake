9820ccd27939544b0a53cd741ab0fb8d1aeef8d4
mtraczyk <67707167+mtraczyk@users.noreply.github.com>
2021-05-19 18:35:16 +0200

    Delete release/googletest directory

--- release/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mac/Desktop/projectsInC++/zadanie2SIK/googletest
-# Build directory: /Users/mac/Desktop/projectsInC++/zadanie2SIK/release/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- release/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mac/Desktop/projectsInC++/zadanie2SIK/googletest/googletest
-# Build directory: /Users/mac/Desktop/projectsInC++/zadanie2SIK/release/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
