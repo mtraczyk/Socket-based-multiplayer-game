0bbd91f1b31bcafbf1c0e5ee054cfe05c079962e
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-05-19 23:33:42 +0200

    More code on polling.

--- server/server.cpp
+++ server/server.cpp
@@ -11,16 +11,23 @@
 #include "err.h"
 
 #define MAX_NUM_OF_PLAYERS 25 // maximum number of players is known
-#define DESC_ARR_SIZE MAX_NUM_OF_PLAYERS + 1 // descriptors' array is of a size MAX_NUM_OF_PLAYERS + 1
+
+/* Descriptors' array is of a size which equals to MAX_NUM_OF_PLAYERS + 2.
+ * One additional is needed for the server's socket, and one more for a turn timer.
+ */
+#define DESC_ARR_SIZE MAX_NUM_OF_PLAYERS + 2
+#define DIS_TIME_SEC 2 // disconnection time in seconds
+#define DIS_TIME_NANO 2 * 1000000000 // disconnection time in nanoseconds
 
 struct pollfd pfds[DESC_ARR_SIZE]; // pollfd array
+nfds_t nfds = DESC_ARR_SIZE; // pfds array's size
 
 namespace {
   void setPollfdArray(int sock) {
     pfds[0].fd = sock; // first descriptor in the array is socket's descriptor
     pfds[0].events = POLLIN; // poll will be done for POLLIN
 
-    for (int i = 1; i < MAX_NUM_OF_PLAYERS; i++) {
+    for (int i = 1; i < DESC_ARR_SIZE; i++) {
       pfds[i].fd = timerfd_create(CLOCK_REALTIME, TFD_NONBLOCK);
       if (pfds[i].fd == -1) {
         syserr("timerfd_create");
@@ -29,16 +36,46 @@ namespace {
       pfds[i].events = POLLIN;
     }
   }
+
+  void checkNextTurn() {
+    if (pfds[DESC_ARR_SIZE - 1].revents != 0) {
+      if (pfds[DESC_ARR_SIZE - 1].revents & POLLIN) {
+        // next turn
+        // set timer again
+      } else { /* POLLERR | POLLHUP */
+        syserr("turn timer error");
+      }
+    }
+  }
+
+  void checkDisconnection() {
+    /* Iterate possibly disconnected clients. */
+    for (int i = 1; i < DESC_ARR_SIZE - 1; i++) {
+      if (pfds[DESC_ARR_SIZE - 1].revents != 0) {
+        if (pfds[DESC_ARR_SIZE - 1].revents & POLLIN) {
+          // disconnected
+          // update number of players
+        } else { /* POLLERR | POLLHUP */
+          syserr("disconnection timer error");
+        }
+      }
+    }
+  }
+
+  void checkDatagram() {
+
+  }
 }
 
 void server(uint32_t portNum, int64_t seed, int64_t turningSpeed,
             uint32_t roundPerSecond, uint32_t boardWidth, uint32_t boardHeight) {
-  int sock;  // socket descriptor
+  int sock; // socket descriptor
+  int ready; // variable to store poll return value
   struct sockaddr_in6 server_address;
   struct sockaddr_in6 client_address;
   socklen_t client_address_len;
 
-  sock = socket(AF_INET6, SOCK_DGRAM | SOCK_NONBLOCK, 0);  // IPv6 UDP nonblock socket
+  sock = socket(AF_INET6, SOCK_DGRAM | SOCK_NONBLOCK, 0); // IPv6 UDP nonblock socket
 
   if (sock < 0) {
     syserr("socket");
@@ -56,7 +93,19 @@ void server(uint32_t portNum, int64_t seed, int64_t turningSpeed,
   client_address_len = sizeof(client_address);
   setPollfdArray(sock); // sets the array that will be used for polling
 
+  for (;;) {
+    ready = poll(pfds, nfds, -1); // wait as long as needed
+    if (ready == -1) {
+      syserr("poll error");
+    }
 
+    /* Checks whether timer for the next round had expired.
+     * If that's the case needed operations are performed.
+     */
+    checkNextTurn();
+    checkDisconnection(); // check for disconnected clients
+    checkDatagram(); // check for something to read in the socket
+  }
 
   if (close(sock) == -1) { // very rare errors can occur here, but then
     syserr("close"); // it's healthy to do the check
