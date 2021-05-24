26df62a3263bbfa5b17a0c1c87d02b63303361ad
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-05-24 15:23:35 +0200

    get tcp socket for a client

--- client/client.cpp
+++ client/client.cpp
@@ -1,6 +1,44 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #include "client.h"
+#include "../err/err.h"
+
+namespace {
+  int getGuiSocket(std::string const &guiServer, uint16_t guiServerPort) {
+    int sock, err;
+    struct addrinfo addrHints;
+    struct addrinfo *addrResult;
+
+    // 'converting' host/port in string to struct addrinfo
+    memset(&addrHints, 0, sizeof(struct addrinfo));
+    addrHints.ai_family = AF_INET6; // IPv6
+    addrHints.ai_socktype = SOCK_STREAM; // TCP
+    addrHints.ai_protocol = IPPROTO_TCP;
+
+    err = getaddrinfo(guiServer.c_str(), std::to_string(guiServerPort).c_str(), &addrHints, &addrResult);
+    if (err == EAI_SYSTEM) { // system error
+      syserr("getaddrinfo: %s", gai_strerror(err));
+    } else if (err != 0) { // other error (host not found, etc.)
+      fatal("getaddrinfo: %s", gai_strerror(err));
+    }
+
+    // initialize socket according to getaddrinfo results
+    sock = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+    if (sock < 0)
+      syserr("socket");
+
+    // connect socket to the server
+    if (connect(sock, addrResult->ai_addr, addrResult->ai_addrlen) < 0)
+      syserr("connect");
+
+    freeaddrinfo(addrResult);
+  }
+}
 
 void client(std::string const &gameServer, uint16_t myPortNum, std::string const &playerName,
             uint16_t gameServerPort, std::string const &guiServer, uint16_t guiServerPort) {
-
+  int guiSocket = getGuiSocket(guiServer, guiServerPort); // obtains socket for client - gui connection
+  (void) close(guiSocket);
 }
